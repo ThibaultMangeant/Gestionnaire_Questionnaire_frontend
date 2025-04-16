810e7e051e4864568dfdd0789ebbae716bff86cb
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-04-16 11:46:27 +0200

    Formulaires créations
    Questionnaire est fonctionnelle, reste à faire les vérif
    
    Questions pas testé et pas vérifié

--- src/views/forms/FormQuestion.vue
+++ src/views/forms/FormQuestion.vue
@@ -1,29 +1,53 @@
 <script setup>
+	import axios from '../../axios.js';
+	import { ref } from 'vue';
 
+	const loading = ref(false);
+
+	const type = ref('');
+	const name = ref('');
+	const content = ref('');
+	const order = ref(0);
+
+	function addQuestion($idQuestionnaire)
+	{
+		loading.value = true;
+
+		loading.value = false;
+	}
 </script>
 
 <template>
 	<v-card>
-		<v-form @submit.prevent>
+		<v-form @submit.prevent="addQuestion($route.params.$idQuestionnaire)">
 			<v-select
+				v-model="type"
+				variant="outlined"
 				label="Type de la question"
 				:items="['Question ouverte', 'Question à choix multiple', 'Vrai/Faux', 'Curseur']">
 			</v-select>
 			<v-text-field
+				v-model="name"
+				variant="outlined"
 				label="Nom de la question"
 				placeholder="Question n°1"
 				clearable>
 			</v-text-field>
 			<v-text-field
+				v-model="content"
+				variant="outlined"
 				label="Ennoncé de la question"
 				placeholder="Quel est la couleur du ciel ?"
 				clearable>
 			</v-text-field>
 			<v-text-field
+				v-model="order"
+				variant="outlined"
 				label="Ordre de la question"
+				type="number"
 				hint="1 = première question du questionnaire, 2 = deuxième, etc.">
 			</v-text-field>
-			<v-btn append-icon="mdi-check-circle" type="submit" block>
+			<v-btn append-icon="mdi-check-circle" type="submit" :loading="loading" block>
 				Créer la question
 			</v-btn>
 		</v-form>
--- src/views/forms/FormQuestionnaire.vue
+++ src/views/forms/FormQuestionnaire.vue
@@ -1,25 +1,58 @@
 <script setup>
+	import axios from '../../axios.js';
+	import { ref } from 'vue';
 
+	const loading = ref(false);
+
+	const usermail = ref('');
+	const name = ref('');
+	const description = ref('');
+
+	function addQuestionnaire()
+	{
+		loading.value = true;
+
+		axios.post('/questionnaires',
+		{
+			usermail: usermail.value,
+			name: name.value,
+			description: description.value
+		})
+		.then(response => {
+			loading.value = false;
+			console.log(response);
+		})
+		.catch(error => {
+			loading.value = false;
+			console.error('Erreur lors de la création du questionnaire :', error);
+		});;
+	}
 </script>
 
 <template>
 	<v-card>
-		<v-form @submit.prevent>
+		<v-form @submit.prevent="addQuestionnaire">
 			<v-text-field
+				v-model="usermail"
+				variant="outlined"
 				label="Adresse Email"
 				placeholder="johndoe@gmail.com"
 				type="email"
 				clearable>
 			</v-text-field>
 			<v-text-field
+				v-model="name"
+				variant="outlined"
 				label="Nom du questionnaire"
 				clearable>
 			</v-text-field>
 			<v-textarea
+				v-model="description"
+				variant="outlined"
 				label="Description du questionnaire"
 				clearable>
 			</v-textarea>
-			<v-btn append-icon="mdi-check-circle" type="submit" block>
+			<v-btn append-icon="mdi-check-circle" type="submit" :loading="loading" block>
 				Créer le questionnaire
 			</v-btn>
 		</v-form>
