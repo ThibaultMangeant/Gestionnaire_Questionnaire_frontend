3e39a97a708dc45758e07042677eb785231a90fa
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-04-17 12:02:23 +0200

    Verif formulaires

--- src/views/forms/FormQuestion.vue
+++ src/views/forms/FormQuestion.vue
@@ -4,10 +4,39 @@
 
 	const loading = ref(false);
 
-	const type = ref('');
+	const form = ref();
+
+	const type = ref(null);
 	const name = ref('');
 	const content = ref('');
 	const order = ref(0);
+
+	const items = ['Question ouverte', 'Question à choix multiple', 'Vrai/Faux', 'Curseur'];
+
+	const typeRules =
+	[
+		value => { return value ? true : 'Vous devez choisir un type de question.'},
+		value => { return items.includes(value) ? true : "Veuillez choisir un type de question parmi ceux disponibles."}
+	];
+
+	const contentRules =
+	[
+		value => { return value ? true : "Vous devez saisir un énoncé de question."}
+	];
+
+	const orderRules =
+	[
+		value => { return value ? true : "Vous devez saisir un ordre."},
+		value => { return value >= 0 ? true : "L'ordre ne peut être négatif"}
+	];
+
+	async function validate($idQuestionnaire)
+	{
+		const { valid } = await form.value.validate();
+
+		if (valid)
+			addQuestion($idQuestionnaire);
+	}
 
 	function addQuestion(idQuestionnaire)
 	{
@@ -35,12 +64,13 @@
 		<v-btn icon="mdi-arrow-left"></v-btn>
 	</RouterLink>
 	<v-card>
-		<v-form @submit.prevent="addQuestion($route.params.idQuestionnaire)">
+		<v-form ref="form">
 			<v-select
 				v-model="type"
+				:rules="typeRules"
 				variant="outlined"
-				label="Type de la question"
-				:items="['Question ouverte', 'Question à choix multiple', 'Vrai/Faux', 'Curseur']">
+				label="Type de la question *"
+				:items="items">
 			</v-select>
 			<v-text-field
 				v-model="name"
@@ -51,21 +81,30 @@
 			</v-text-field>
 			<v-text-field
 				v-model="content"
+				:rules="contentRules"
 				variant="outlined"
-				label="Ennoncé de la question"
+				label="Énoncé de la question *"
 				placeholder="Quel est la couleur du ciel ?"
 				clearable>
 			</v-text-field>
 			<v-text-field
 				v-model="order"
+				:rules="orderRules"
 				variant="outlined"
-				label="Ordre de la question"
+				label="Ordre de la question *"
 				type="number"
-				hint="1 = première question du questionnaire, 2 = deuxième, etc.">
+				min="0"
+				step="1"
+				hint="Le numéro indique la position dans le questionnaire (ex: 0 = Pas d'ordre, 1 = Première position)">
 			</v-text-field>
-			<v-btn append-icon="mdi-check-circle" type="submit" :loading="loading" block>
+			<v-btn @click.prevent="validate($route.params.idQuestionnaire)"
+				append-icon="mdi-check-circle"
+				type="submit"
+				:loading="loading"
+				block>
 				Créer la question
 			</v-btn>
 		</v-form>
 	</v-card>
+	<p class="text-red">Les champs marqués d'un astérix (*) sont obligatoires.</p>
 </template>
--- src/views/forms/FormQuestionnaire.vue
+++ src/views/forms/FormQuestionnaire.vue
@@ -4,9 +4,30 @@
 
 	const loading = ref(false);
 
+	const form = ref();
+
 	const usermail = ref('');
 	const name = ref('');
 	const description = ref('');
+
+	const emailRules =
+	[
+		value => { return value ? true : 'Vous devez saisir une adresse mail.'},
+		value => { return /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(value) ? true : "L'adresse email doit être valide"}
+	];
+
+	const nameRules =
+	[
+		value => { return value ? true : "Vous devez saisir un nom de questionnaire."}
+	]
+
+	async function validate()
+	{
+		const { valid } = await form.value.validate();
+
+		if (valid)
+			addQuestionnaire();
+	}
 
 	function addQuestionnaire()
 	{
@@ -34,20 +55,24 @@
 		<v-btn icon="mdi-arrow-left"></v-btn>
 	</RouterLink>
 	<v-card>
-		<v-form @submit.prevent="addQuestionnaire">
+		<v-form ref="form">
 			<v-text-field
 				v-model="usermail"
+				:rules="emailRules"
 				variant="outlined"
-				label="Adresse Email"
+				label="Adresse Email *"
 				placeholder="johndoe@gmail.com"
 				type="email"
-				clearable>
+				clearable
+				required>
 			</v-text-field>
 			<v-text-field
 				v-model="name"
+				:rules="nameRules"
 				variant="outlined"
-				label="Nom du questionnaire"
-				clearable>
+				label="Nom du questionnaire *"
+				clearable
+				required>
 			</v-text-field>
 			<v-textarea
 				v-model="description"
@@ -55,9 +80,14 @@
 				label="Description du questionnaire"
 				clearable>
 			</v-textarea>
-			<v-btn append-icon="mdi-check-circle" type="submit" :loading="loading" block>
+			<v-btn @click.prevent="validate"
+				append-icon="mdi-check-circle"
+				type="submit"
+				:loading="loading"
+				block>
 				Créer le questionnaire
 			</v-btn>
 		</v-form>
 	</v-card>
+	<p class="text-red">Les champs marqués d'un astérix (*) sont obligatoires.</p>
 </template>
